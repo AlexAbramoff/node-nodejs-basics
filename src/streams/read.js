9589172213a7b55519434425c621541b2e087841
Alexey Abramov <alex93178@gmail.com>
2022-06-05 21:16:04 +0300

    feat: implement streams functions

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,3 +1,10 @@
+import process from 'process';
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
 export const read = async () => {
-    // Write your code here 
+    const __filename = fileURLToPath(import.meta.url)
+    const __dirname = path.dirname(__filename)
+    fs.createReadStream(path.join(__dirname, 'files', 'fileToRead.txt'),'utf8').pipe(process.stdout)
 };
--- src/streams/write.js
+++ src/streams/write.js
@@ -1,3 +1,12 @@
+import process from 'process';
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
 export const write = async () => {
-    // Write your code here 
+    const __filename = fileURLToPath(import.meta.url)
+    const __dirname = path.dirname(__filename)
+    const writeStr=fs.createWriteStream(path.join(__dirname,'files','fileToWrite.txt'))
+    process.stdin.pipe(writeStr)
 };
+write()
